c5003be4e53da99e51a4cc1144d14190c339a1a1
Avi <abavisetty3@gmail.com>
2024-04-11 03:26:06 -0700

    -commented out code in PortfolioFragment.java - added pictures to drawables folder - added scrolling on the fragment_portfolio.xml what it needs to be fixed still: displaying the first picture properly from the beginning. will do later.

--- app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/portfolio/PortfolioFragment.java
+++ app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/portfolio/PortfolioFragment.java
@@ -24,8 +24,8 @@ public class PortfolioFragment extends Fragment {
         binding = FragmentPortfolioBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textPortfolio;
-        portfolioViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textPortfolio;
+//        portfolioViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 

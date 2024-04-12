f465a3a80807bdd85e00f85429fbf671e0d7076c
HassanBFarhat <farhathassan@rocketmail.com>
2024-04-11 19:02:42 -0700

    10th: Changed the application name to clients actual shop name. Added the map with the location of the shop shown by a marker in the shop details fragment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.activity)
     implementation(libs.legacy.support.v4)
+    implementation (libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/details/DetailsFragment.java
+++ app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/details/DetailsFragment.java
@@ -1,32 +1,55 @@
 package edu.tacoma.uw.barber_mobile_application.ui.details;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+import edu.tacoma.uw.barber_mobile_application.R;
 import edu.tacoma.uw.barber_mobile_application.databinding.FragmentDetailsBinding;
 
 public class DetailsFragment extends Fragment {
 
+    private GoogleMap mMap;
     private FragmentDetailsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DetailsViewModel detailsViewModel =
-                new ViewModelProvider(this).get(DetailsViewModel.class);
 
-        binding = FragmentDetailsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
 
-        final TextView textView = binding.textDetails;
-        detailsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        assert mapFragment != null;
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                mMap = googleMap;
+                String address = "33100 Pacific Hwy S, Suite 10, Federal Way, Wa 98003";
+                LatLng location = getLocationFromAddress(address);
+
+                if (location != null) {
+                    mMap.addMarker(new MarkerOptions().position(location).title("The Empire Barbershop"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                }
+            }
+        });
+
+        return view;
     }
 
     @Override
@@ -34,4 +57,28 @@ public class DetailsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    // Helper Method
+    private LatLng getLocationFromAddress(String strAddress) {
+        Geocoder coder = new Geocoder(getContext());
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null || address.isEmpty()) {
+                return null; // Address not found
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return p1;
+    }
+
 }

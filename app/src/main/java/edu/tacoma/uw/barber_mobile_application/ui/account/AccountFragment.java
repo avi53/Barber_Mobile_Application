8a840028ac1a26e28795d96e67836b09b35e491e
HassanBFarhat <farhathassan@rocketmail.com>
2024-05-01 09:48:06 -0700

    22nd: Added toast functionality to the various buttons on the account fragment page. Also changed the id's for the buttons on the account xml page.

--- app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/account/AccountFragment.java
+++ app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/account/AccountFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import edu.tacoma.uw.barber_mobile_application.R;
 import edu.tacoma.uw.barber_mobile_application.databinding.FragmentAccountBinding;
 
 public class AccountFragment extends Fragment {
@@ -27,6 +31,35 @@ public class AccountFragment extends Fragment {
 //        final TextView textView = binding.textAccount;
 //        accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button editBtn = view.findViewById(R.id.edit_btn);
+        Button cardBtn = view.findViewById(R.id.card_details_btn);
+        Button logoutBtn = view.findViewById(R.id.logout_btn);
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Will take user to edit account information", Toast.LENGTH_SHORT).show();
+            }
+        });
+        cardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Will take user Square page to add cards safely", Toast.LENGTH_SHORT).show();
+            }
+        });
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Will log the user out", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override

5a9fac3027da5604c729fa20c7dee27534893b13
HassanBFarhat <farhathassan@rocketmail.com>
2024-05-01 11:02:55 -0700

    23rd: Refactored by placing some files in proper services directory

--- app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/services/ServicesFragment.java
+++ app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/services/ServicesFragment.java
@@ -4,15 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
-import edu.tacoma.uw.barber_mobile_application.OptionsFragment;
 import edu.tacoma.uw.barber_mobile_application.R;
 import edu.tacoma.uw.barber_mobile_application.databinding.FragmentServicesBinding;
 

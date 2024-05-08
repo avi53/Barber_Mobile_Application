ab6a1a0193932f04c193771ee849b6a0db11eaad
HassanBFarhat <farhathassan@rocketmail.com>
2024-05-07 19:38:08 -0700

    26th: Added both Login and Registration fragments. Added all appropriate XML design for both pages. (Working on actual implementation/incorporation to the applications functionality)

---
+++ app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/account/RegistrationFragment.java
@@ -0,0 +1,24 @@
+package edu.tacoma.uw.barber_mobile_application.ui.account;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.tacoma.uw.barber_mobile_application.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegistrationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+}
--- app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/details/AboutFragment.java
+++ app/src/main/java/edu/tacoma/uw/barber_mobile_application/ui/details/AboutFragment.java
@@ -1,4 +1,4 @@
-package edu.tacoma.uw.barber_mobile_application;
+package edu.tacoma.uw.barber_mobile_application.ui.details;
 
 import android.os.Bundle;
 
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import edu.tacoma.uw.barber_mobile_application.R;
 
 /**
  * A simple {@link Fragment} subclass.
